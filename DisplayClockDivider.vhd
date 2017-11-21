fea84026e764adeb7627c59e3b9bd3fb21d8d6f6
Guilherme Sehn <ghsehn@gmail.com>
2017-11-21 11:46:44 -0200

    make counter of display clock divider start from 0

--- DisplayClockDivider.vhd
+++ DisplayClockDivider.vhd
@@ -10,7 +10,7 @@ entity DisplayClockDivider is
 end DisplayClockDivider;
 
 architecture Behavioral of DisplayClockDivider is
-  signal counter : integer range 1 to 1000 := 1;
+  signal counter : integer range 0 to 1000 := 0;
   signal internal_clock : std_logic := '0';
 begin
   divided_clock <= internal_clock;
@@ -20,7 +20,7 @@ begin
     if (clock'event and clock = '1') then
       counter <= counter + 1;
       if counter = 1000 then
-        counter <= 1;
+        counter <= 0;
         internal_clock <= not internal_clock;
       end if;
     end if;
